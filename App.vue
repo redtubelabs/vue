e0fcefb55eb7d3b24a12864a0745d8145480390f
Vitor Leonel <falecom@vitorleonel.com>
2018-12-09 04:32:30 -0200

    init dark mode

--- App.vue
+++ App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Wrapper>
+  <Wrapper :class="{'dark-theme': theme === 'dark'}">
     <template slot-scope="{ videos }">
       <transition name="list">
         <Modal v-if="isModalVisible">
@@ -18,34 +18,56 @@
 </template>
 
 <script>
-import Wrapper from './components/Wrapper'
-import Card from './components/Card'
-import Modal from './components/Modal'
-import Embed from './components/Embed'
+import Wrapper from './components/Wrapper';
+import Card from './components/Card';
+import Modal from './components/Modal';
+import Embed from './components/Embed';
 
 export default {
   name: 'VueRedTube',
   components: { Wrapper, Card, Modal, Embed },
   data: () => ({
     currentVideo: {},
-    isModalVisible: false
+    isModalVisible: false,
+    theme: 'light',
   }),
+  mounted() {
+    this.getTheme()
+  },
   methods: {
     play ({ video }) {
       this.currentVideo = video
       this.isModalVisible = true
       window.scrollTo(0, 0)
+    },
+    getTheme () {
+      const theme = window.localStorage.getItem('vue:theme')
+
+      if(theme) {
+        this.theme = theme
+      }
     }
   }
-}
+};
 </script>
 
 <style lang="scss">
-* { margin: 0; padding: 0; box-sizing: border-box; }
-html, body {
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html,
+body {
   width: 100%;
   height: 100%;
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
+    'Lucida Sans', Arial, sans-serif;
+}
+
+.dark-theme {
+  background-color: #2f3640;
 }
 
 .list-container {
@@ -59,10 +81,12 @@ html, body {
 .list-item {
   margin-right: 10px;
 }
-.list-enter-active, .list-leave-active {
+.list-enter-active,
+.list-leave-active {
   transition: all 1s;
 }
-.list-enter, .list-leave-to {
+.list-enter,
+.list-leave-to {
   opacity: 0;
   transform: translateY(30px);
 }
